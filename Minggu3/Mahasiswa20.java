00085f718a292a893120decf7fb14a11ec33c4ae
pacifiista <naurasfh2020@gmail.com>
2026-02-27 12:28:23 +0700

    update

--- Minggu3/Mahasiswa20.java
+++ Minggu3/Mahasiswa20.java
@@ -5,9 +5,18 @@ public class Mahasiswa20 {
     public String kelas;
     public float ipk;
 
+    // method untuk menampilkan data mahasiswa
+    public void tampilkanDataMahasiswa(Mahasiswa20[] arrayOfMahasiswa) {
+    for (int i = 0; i < 3; i++) {
+                System.out.println("Data Mahasiswa ke-" + (i + 1) + ":");
+                System.out.println("NIM: " + arrayOfMahasiswa[i].nim);
+                System.out.println("Nama: " + arrayOfMahasiswa[i].nama);
+                System.out.println("Kelas: " + arrayOfMahasiswa[i].kelas);
+                System.out.println("IPK: " + arrayOfMahasiswa[i].ipk);
+                System.out.println("------------------------------");
+            }
 
 
-
-
+        }
 
 }
